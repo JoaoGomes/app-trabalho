63239c7113698dc28922d98742e26f36687d678c
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-15 15:45:22 -0300

    Ligação com o Banco de Dados local.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
     public function index(){
-        return view('produto.index');
+        $produtos = Produto::all();
+        return view('produto.index', ['prods' => $produtos]);
     }
 
     public function view($nome){
